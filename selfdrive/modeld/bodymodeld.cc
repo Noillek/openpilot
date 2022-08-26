f40e12624a703437a1e4e674830b0999ba3a0db5
mitchellgoffpc <mitchellgoffpc@gmail.com>
2022-08-26 12:03:32 -0700

    Submaster test

--- selfdrive/modeld/bodymodeld.cc
+++ selfdrive/modeld/bodymodeld.cc
@@ -4,68 +4,22 @@
 #include <cstdio>
 #include <cstdlib>
 
-#include "cereal/visionipc/visionipc_client.h"
+#include "cereal/messaging/messaging.h"
 #include "common/swaglog.h"
 #include "common/util.h"
-#include "selfdrive/modeld/models/body.h"
 
 ExitHandler do_exit;
 
-void run_model(BodyModelState &model, VisionIpcClient &vipc_client) {
-  PubMaster pm({"driverStateV2"});
+void run_model() {
   SubMaster sm({"carState", "carControl"});
-  float inputs[INPUT_SIZE];
 
   while (!do_exit) {
-    VisionIpcBufExtra extra = {};
-    VisionBuf *buf = vipc_client.recv(&extra);
-    if (buf == nullptr) continue;
-
-    sm.update(0);
-    if (sm.updated("carState")) {
-      inputs[0] = sm["carState"].getCarState().getWheelSpeeds().getFl();
-      inputs[1] = sm["carState"].getCarState().getWheelSpeeds().getFr();
-      inputs[2] = sm["carState"].getCarState().getVEgo();
-      inputs[3] = sm["carState"].getCarState().getAEgo();
-    }
-    if (sm.updated("carControl")) {
-      auto orientation = sm["carControl"].getCarControl().getOrientationNED();
-      auto angular_velocity = sm["carControl"].getCarControl().getAngularVelocity();
-      if (orientation.size() >= 2) { inputs[4] = orientation[1]; }
-      if (angular_velocity.size() >= 2) { inputs[5] = angular_velocity[1]; }
-    }
-
-    BodyModelResult res = bodymodel_eval_frame(&model, inputs);
-
-    // send dm packet
-    MessageBuilder msg;
-    auto framed = msg.initEvent().initDriverStateV2();
-    auto data = framed.initLeftDriverData();
-    framed.setFrameId(extra.frame_id);
-    data.setLeftEyeProb(res.torque_left);
-    data.setRightEyeProb(res.torque_right);
-    pm.send("driverStateV2", msg);
+    sm.update(100);
   }
 }
 
 int main(int argc, char **argv) {
   setpriority(PRIO_PROCESS, 0, -15);
-
-  // init the models
-  BodyModelState model;
-  bodymodel_init(&model);
-
-  VisionIpcClient vipc_client = VisionIpcClient("camerad", VISION_STREAM_WIDE_ROAD, true);
-  while (!do_exit && !vipc_client.connect(false)) {
-    util::sleep_for(100);
-  }
-
-  // run the models
-  if (vipc_client.connected) {
-    LOGW("connected with buffer size: %d", vipc_client.buffers[0].len);
-    run_model(model, vipc_client);
-  }
-
-  bodymodel_free(&model);
+  run_model();
   return 0;
 }
